1619e20d10822fc42cf2f6e4c5fc717884dbf6ca
Graham Kang <graham.kang@kakaocorp.com>
2016-03-03 22:22:23 +0900

    JUnit 테스트로 전환.

---
+++ src/springbook/user/Application.java
@@ -0,0 +1,12 @@
+package springbook.user;
+
+import org.junit.runner.JUnitCore;
+
+/**
+ * Created by graham on 2016. 3. 3..
+ */
+public class Application {
+    public static void main(String[] args) {
+        JUnitCore.main("springbook.user.UserDaoTest");
+    }
+}
