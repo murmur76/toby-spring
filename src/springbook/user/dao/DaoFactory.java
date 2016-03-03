a97dffff7ea78d5d6248c06b35d37fd5ec71dbfa
Graham Kang <graham.kang@kakaocorp.com>
2016-03-03 21:06:51 +0900

    DAO 생성 메소드의 추가로 인해 발생하는 중복 예시.

--- src/springbook/user/dao/DaoFactory.java
+++ src/springbook/user/dao/DaoFactory.java
@@ -7,4 +7,16 @@ public class DaoFactory {
     public UserDao userDao() {
         return new UserDao(new DConnectionMaker());
     }
+
+    /* ConnectionMaker 구현 클래스를 선정하고 생성하는 코드의 중복
+
+    public AccountDao accountDao() {
+        return new AccountDao(new DConnectionMaker());
+    }
+
+    public MessageDao messageDao() {
+        return new MessageDao(new DConnectionMaker());
+    }
+
+    */
 }
