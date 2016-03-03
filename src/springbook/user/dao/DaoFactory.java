1c0884925b18f919063a14cedb615198bfbd5571
Graham Kang <graham.kang@kakaocorp.com>
2016-03-03 21:08:38 +0900

    생성 오브젝트 코드 수정.

--- src/springbook/user/dao/DaoFactory.java
+++ src/springbook/user/dao/DaoFactory.java
@@ -5,18 +5,22 @@ package springbook.user.dao;
  */
 public class DaoFactory {
     public UserDao userDao() {
-        return new UserDao(new DConnectionMaker());
+        return new UserDao(connectionMaker());
     }
 
     /* ConnectionMaker 구현 클래스를 선정하고 생성하는 코드의 중복
 
     public AccountDao accountDao() {
-        return new AccountDao(new DConnectionMaker());
+        return new AccountDao(connectionMaker());
     }
 
     public MessageDao messageDao() {
-        return new MessageDao(new DConnectionMaker());
+        return new MessageDao(connectionMaker());
     }
 
     */
+
+    public ConnectionMaker connectionMaker() {
+        return new DConnectionMaker();
+    }
 }
