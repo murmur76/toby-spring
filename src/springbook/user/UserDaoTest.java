c99889dd3aef1087ad2875737631d8e9b326315e
Graham Kang <graham.kang@kakaocorp.com>
2016-03-03 22:44:21 +0900

    UserDaoTest addAndGet 테스트 개선.

--- src/springbook/user/UserDaoTest.java
+++ src/springbook/user/UserDaoTest.java
@@ -20,16 +20,23 @@ public class UserDaoTest {
         ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
         UserDao dao = context.getBean("userDao", UserDao.class);
 
+        User user1 = new User("gyumee", "박성철", "springno1");
+        User user2 = new User("leegw700", "이길원", "springno2");
+
         dao.deleteAll();
         assertEquals(dao.getCount(), 0);
 
-        User user = new User("whiteship", "graham", "kang");
-        dao.add(user);
-        assertEquals(dao.getCount(), 1);
+        dao.add(user1);
+        dao.add(user2);
+        assertEquals(dao.getCount(), 2);
 
-        User user2 = dao.get(user.getId());
-        assertEquals(user.getName(), user2.getName());
-        assertEquals(user.getPassword(), user2.getPassword());
+        User userget1 = dao.get(user1.getId());
+        assertEquals(userget1.getName(), user1.getName());
+        assertEquals(userget1.getPassword(), user1.getPassword());
+
+        User userget2 = dao.get(user2.getId());
+        assertEquals(userget2.getName(), user2.getName());
+        assertEquals(userget2.getPassword(), user2.getPassword());
     }
 
     @Test
