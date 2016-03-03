fd2b3928ca0231503d0bad2ad81b99461ebf3103
Graham Kang <graham.kang@kakaocorp.com>
2016-03-03 20:56:26 +0900

    UserDao 생성자 수정.
    관계설정 책임의 분리

--- src/springbook/user/UserDaoTest.java
+++ src/springbook/user/UserDaoTest.java
@@ -1,5 +1,7 @@
 package springbook.user;
 
+import springbook.user.dao.ConnectionMaker;
+import springbook.user.dao.DConnectionMaker;
 import springbook.user.dao.UserDao;
 import springbook.user.domain.User;
 
@@ -8,7 +10,8 @@ import java.sql.SQLException;
 public class UserDaoTest {
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        UserDao dao = new UserDao();
+        ConnectionMaker connectionMaker = new DConnectionMaker();
+        UserDao dao = new UserDao(connectionMaker);
 
         User user = new User();
         user.setId("whiteship");
