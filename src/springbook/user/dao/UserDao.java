6d5e948e520086a418d71fa40da70f84db1354d9
Graham Kang <graham.kang@kakaocorp.com>
2016-03-07 14:11:35 +0900

    3.6.2 JdbcTemplate query().

--- src/springbook/user/dao/UserDao.java
+++ src/springbook/user/dao/UserDao.java
@@ -2,8 +2,11 @@ package springbook.user.dao;
 
 
 
+import org.springframework.dao.DataAccessException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.ResultSetExtractor;
 import springbook.user.domain.User;
 
 import javax.sql.DataSource;
@@ -22,25 +25,23 @@ public class UserDao {
         jdbcTemplate.update("insert into users(id, name, password) values(?,?,?)", new Object[]{ user.getId(), user.getName(), user.getPassword() });
     }
 
-    public User get(String id) throws ClassNotFoundException, SQLException {
-        Connection c = dataSource.getConnection();
-
-        PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
-        ps.setString(1, id);
-
-        ResultSet rs = ps.executeQuery();
-
-        User user = null;
-        if (rs.next()) {
-            user = new User(rs.getString("id"), rs.getString("name"), rs.getString("password"));
-        }
-
-        rs.close();
-        ps.close();
-        c.close();
-
-        if (user == null) throw new EmptyResultDataAccessException(1);
-        return user;
+    public User get(final String id) throws ClassNotFoundException, SQLException {
+        return jdbcTemplate.query(new PreparedStatementCreator() {
+            public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
+                PreparedStatement ps = connection.prepareStatement("select * from users where id = ?");
+                ps.setString(1, id);
+                return ps;
+            }
+        }, new ResultSetExtractor<User>() {
+            public User extractData(ResultSet rs) throws SQLException, DataAccessException {
+                User user = null;
+                if (rs.next()) {
+                    user = new User(rs.getString("id"), rs.getString("name"), rs.getString("password"));
+                }
+                if (user == null) throw new EmptyResultDataAccessException(1);
+                return user;
+            }
+        });
     }
 
     public void deleteAll() throws SQLException {
@@ -48,23 +49,12 @@ public class UserDao {
     }
 
     public int getCount() throws SQLException {
-        Connection c = null;
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-
-        try {
-            c = dataSource.getConnection();
-            ps = c.prepareStatement("select count(*) from users");
-            rs = ps.executeQuery();
-            rs.next();
-            return rs.getInt(1);
-        } catch (SQLException e) {
-            throw e;
-        } finally {
-            if (rs != null) { try { rs.close(); } catch (SQLException e) { } }
-            if (ps != null) { try { ps.close(); } catch (SQLException e) { } }
-            if (c != null) { try { c.close(); } catch (SQLException e) { } }
-        }
+        return jdbcTemplate.query("select count(*) from users", new ResultSetExtractor<Integer>() {
+            public Integer extractData(ResultSet rs) throws SQLException, DataAccessException {
+                rs.next();
+                return rs.getInt(1);
+            }
+        });
     }
 
 }
