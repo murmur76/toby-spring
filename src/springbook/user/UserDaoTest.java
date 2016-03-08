c628b507b318e1caaafa0c40be003662263fd920
Graham Kang <graham.kang@kakaocorp.com>
2016-03-08 22:34:51 +0900

    Remove unused imports.

--- src/springbook/user/UserDaoTest.java
+++ src/springbook/user/UserDaoTest.java
@@ -3,19 +3,15 @@ package springbook.user;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.jdbc.datasource.SingleConnectionDataSource;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import springbook.user.dao.UserDao;
 import springbook.user.domain.User;
 
-import javax.sql.DataSource;
 import java.sql.SQLException;
 import java.util.List;
 
