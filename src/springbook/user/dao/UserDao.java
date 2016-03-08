7f138a6172b60ac400f2b00784743f649430c1e7
Graham Kang <graham.kang@kakaocorp.com>
2016-03-08 22:34:25 +0900

    UserDao 인터페이스 분리.

--- src/springbook/user/dao/UserDao.java
+++ src/springbook/user/dao/UserDao.java
@@ -1,52 +1,16 @@
 package springbook.user.dao;
 
-
-
-import org.springframework.dao.DataAccessException;
-import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementCreator;
-import org.springframework.jdbc.core.ResultSetExtractor;
-import org.springframework.jdbc.core.RowMapper;
 import springbook.user.domain.User;
 
-import javax.sql.DataSource;
-import java.sql.*;
 import java.util.List;
 
-public class UserDao {
-    private JdbcTemplate jdbcTemplate;
-    private DataSource dataSource;
-    private RowMapper<User> userMapper = new RowMapper<User>() {
-        public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-            return new User(rs.getString("id"), rs.getString("name"), rs.getString("password"));
-        }
-    };
-
-    public void setDataSource(DataSource dataSource) {
-        this.jdbcTemplate = new JdbcTemplate(dataSource);
-        this.dataSource = dataSource;
-    }
-
-    public void add(final User user) {
-        jdbcTemplate.update("insert into users(id, name, password) values(?,?,?)", new Object[]{ user.getId(), user.getName(), user.getPassword() });
-    }
-
-    public User get(final String id) {
-        return jdbcTemplate.queryForObject("select * from users where id = ?",
-                new Object[] { id }, userMapper);
-    }
-
-    public List<User> getAll() {
-        return jdbcTemplate.query("select * from users order by id", userMapper);
-    }
-
-    public void deleteAll() {
-        jdbcTemplate.update("delete from users");
-    }
-
-    public int getCount() {
-        return jdbcTemplate.queryForObject("select count(*) from users", Integer.class);
-    }
-
+/**
+ * Created by graham on 2016. 3. 8..
+ */
+public interface UserDao {
+    void add(User User);
+    User get(String id);
+    List<User> getAll();
+    void deleteAll();
+    int getCount();
 }
