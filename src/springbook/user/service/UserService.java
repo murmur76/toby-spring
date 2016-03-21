f4ee61ad11946da7315a1192557b51d8e7268c6d
Graham Kang <graham.kang@kakaocorp.com>
2016-03-21 11:00:57 +0900

    Transactional 어노테이션.

--- src/springbook/user/service/UserService.java
+++ src/springbook/user/service/UserService.java
@@ -1,11 +1,15 @@
 package springbook.user.service;
 
+import org.springframework.transaction.annotation.Transactional;
 import springbook.user.domain.User;
 
 /**
  * Created by graham on 2016. 3. 14..
  */
 public interface UserService {
+    @Transactional
     void add(User user);
+
+    @Transactional
     void upgradeLevels();
 }
