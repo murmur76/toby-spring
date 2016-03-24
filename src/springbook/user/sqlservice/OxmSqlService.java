cd55e68f7c2af2009f3336d79fb4604fdb136902
Graham Kang <graham.kang@kakaocorp.com>
2016-03-24 21:46:38 +0900

    7.3.2 OXM 서비스 추상화 적용.

---
+++ src/springbook/user/sqlservice/OxmSqlService.java
@@ -0,0 +1,71 @@
+package springbook.user.sqlservice;
+
+import org.springframework.core.io.Resource;
+import org.springframework.oxm.Unmarshaller;
+import springbook.user.sqlservice.jaxb.SqlType;
+import springbook.user.sqlservice.jaxb.Sqlmap;
+
+import javax.annotation.PostConstruct;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import java.io.IOException;
+
+public class OxmSqlService implements SqlService {
+    private final BaseSqlService baseSqlService = new BaseSqlService();
+    private final OxmSqlReader oxmSqlReader = new OxmSqlReader();
+
+    private SqlRegistry sqlRegistry = new HashMapSqlRegistry();
+
+    public void setSqlRegistry(SqlRegistry sqlRegistry) {
+        this.sqlRegistry = sqlRegistry;
+    }
+
+    public void setUnmarshaller(Unmarshaller unmarshaller) {
+        this.oxmSqlReader.unmarshaller = unmarshaller;
+    }
+
+    public void setSqlmap(Resource sqlmap) {
+        this.oxmSqlReader.setSqlmap(sqlmap);
+    }
+
+    @PostConstruct
+    public void loadSql() {
+        this.baseSqlService.setSqlReader(this.oxmSqlReader);
+        this.baseSqlService.setSqlRegistry(this.sqlRegistry);
+        this.baseSqlService.loadSql();
+    }
+
+    public String getSql(String key) throws SqlRetrievalFailureException {
+        return this.baseSqlService.getSql(key);
+    }
+
+    private class OxmSqlReader implements SqlReader {
+        private Resource sqlmap;
+
+        public void setSqlmap(Resource sqlmap) {
+            this.sqlmap = sqlmap;
+        }
+
+        private Unmarshaller unmarshaller;
+
+        public void setUnmarshaller(Unmarshaller unmarshaller) {
+            this.unmarshaller = unmarshaller;
+        }
+
+        public void read(SqlRegistry sqlRegistry) {
+            try {
+                Source source = new StreamSource(sqlmap.getInputStream());
+                Sqlmap sqlmap = (Sqlmap) this.unmarshaller.unmarshal(source);
+                for (SqlType sql : sqlmap.getSql()) {
+                    sqlRegistry.registerSql(sql.getKey(), sql.getValue());
+                }
+            } catch (IOException e) {
+                throw new IllegalArgumentException(this.sqlmap.getFilename()
+                        + "을 가져올 수 없습니다.", e);
+            }
+
+        }
+
+    }
+
+}
